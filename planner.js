8c1bb6711025a16f842713e354d4f9cae064d874
Jefferson Santos <117351991+SantsJeff@users.noreply.github.com>
2023-05-01 22:18:46 -0300

    Update planner.js

--- planner.js
+++ planner.js
@@ -1,9 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  // configurações do seu projeto Firebase
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 // Selecionando todos os formulários na página
 const forms = document.querySelectorAll('form');
 
 // Adicionando um evento de envio de formulário para cada formulário
 forms.forEach(form => {
-  form.addEventListener('submit', event => {
+  form.addEventListener('submit', async event => {
     // Previne o envio do formulário padrão
     event.preventDefault();
 
@@ -38,50 +48,18 @@ forms.forEach(form => {
     // Adiciona a li à lista ul
     ul.appendChild(li);
 
+    // Adiciona um novo documento à coleção "tarefas" do Firestore com o valor do input
+    try {
+      const docRef = await addDoc(collection(db, "tarefas"), {
+        nome: value,
+        feito: false
+      });
+      console.log("Documento adicionado com ID: ", docRef.id);
+    } catch (e) {
+      console.error("Erro ao adicionar documento: ", e);
+    }
+
     // Limpa o input do formulário
     input.value = '';
   });
 });
-
-// Adicionando um evento de clique a todos os botões de remover na página
-const buttons = document.querySelectorAll('.remover');
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    button.closest('li').remove();
-  });
-});
-
-window.addEventListener('scroll', function() {
-  const btnTopo = document.getElementById('btnTopo');
-  if (window.pageYOffset > 100) {
-    btnTopo.style.display = "block";
-  } else {
-    btnTopo.style.display = "none";
-  }
-});
-
-document.getElementById('btnTopo').addEventListener('click', function() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
-});
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDBuv_hfXJaj2Gn2lzOVB97gwL7nBKMWGs",
-  authDomain: "nossoplanner.firebaseapp.com",
-  projectId: "nossoplanner",
-  storageBucket: "nossoplanner.appspot.com",
-  messagingSenderId: "380508014454",
-  appId: "1:380508014454:web:829bfcd85913f251a09c57",
-  measurementId: "G-4W7H1KEE2Y"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
