ef9ce683d2cf3e29803140a6050db61418da33a3
Jefferson Santos <117351991+SantsJeff@users.noreply.github.com>
2023-05-02 13:11:55 -0300

    Update planner.js

--- planner.js
+++ planner.js
@@ -1,59 +1,36 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+// Seleciona o input e o valor do texto inserido nele
+const input = form.querySelector('input[type="text"]');
+const value = input.value;
 
-const firebaseConfig = {
-  // configurações do seu projeto Firebase
-};
+// Salva o valor no localStorage
+localStorage.setItem('inputValue', value);
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+// Seleciona a seção pai e a lista ul
+const section = form.closest('section');
+const ul = section.querySelector('ul');
 
-function adicionarItem(form) {
-  form.addEventListener('submit', async event => {
-    event.preventDefault();
+// Cria um novo elemento li, um checkbox e um rótulo para ele
+const li = document.createElement('li');
+const checkbox = document.createElement('input');
+checkbox.type = 'checkbox';
+const label = document.createElement('label');
+label.textContent = value;
 
-    const input = form.querySelector('input[type="text"]');
-    const value = input.value.trim();
+// Cria um botão de remoção e adiciona um evento de clique a ele
+const button = document.createElement('button');
+button.textContent = 'Remover';
+button.classList.add('remover');
+button.addEventListener('click', () => {
+  li.remove();
+});
 
-    if (!value) {
-      return;
-    }
+// Adiciona o checkbox, o rótulo e o botão à li
+li.appendChild(checkbox);
+li.appendChild(label);
+li.appendChild(button);
 
-    const section = form.closest('section');
-    const ul = section.querySelector('ul');
+// Adiciona a li à lista ul
+ul.appendChild(li);
 
-    const li = document.createElement('li');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    const label = document.createElement('label');
-    label.textContent = value;
-
-    const button = document.createElement('button');
-    button.textContent = 'Remover';
-    button.classList.add('remover');
-    button.addEventListener('click', () => {
-      li.remove();
-    });
-
-    li.appendChild(checkbox);
-    li.appendChild(label);
-    li.appendChild(button);
-
-    ul.appendChild(li);
-
-    try {
-      const docRef = await addDoc(collection(db, "tarefas"), {
-        nome: value,
-        feito: false
-      });
-      console.log("Documento adicionado com ID: ", docRef.id);
-    } catch (e) {
-      console.error("Erro ao adicionar documento: ", e);
-    }
-
-    input.value = '';
-  });
-}
-
-const forms = document.querySelectorAll('form');
-forms.forEach(adicionarItem);
+// Limpa o input do formulário
+input.value = '';
