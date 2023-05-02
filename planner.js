27ece9e931d0a093becb76d71aaaf3dbd348a417
Jefferson Santos <117351991+SantsJeff@users.noreply.github.com>
2023-05-01 22:27:54 -0300

    Update planner.js

--- planner.js
+++ planner.js
@@ -8,31 +8,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Selecionando todos os formulários na página
-const forms = document.querySelectorAll('form');
-
-// Adicionando um evento de envio de formulário para cada formulário
-forms.forEach(form => {
+function adicionarItem(form) {
   form.addEventListener('submit', async event => {
-    // Previne o envio do formulário padrão
     event.preventDefault();
 
-    // Seleciona o input e o valor do texto inserido nele
     const input = form.querySelector('input[type="text"]');
-    const value = input.value;
+    const value = input.value.trim();
 
-    // Seleciona a seção pai e a lista ul
+    if (!value) {
+      return;
+    }
+
     const section = form.closest('section');
     const ul = section.querySelector('ul');
 
-    // Cria um novo elemento li, um checkbox e um rótulo para ele
     const li = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     const label = document.createElement('label');
     label.textContent = value;
 
-    // Cria um botão de remoção e adiciona um evento de clique a ele
     const button = document.createElement('button');
     button.textContent = 'Remover';
     button.classList.add('remover');
@@ -40,15 +35,12 @@ forms.forEach(form => {
       li.remove();
     });
 
-    // Adiciona o checkbox, o rótulo e o botão à li
     li.appendChild(checkbox);
     li.appendChild(label);
     li.appendChild(button);
 
-    // Adiciona a li à lista ul
     ul.appendChild(li);
 
-    // Adiciona um novo documento à coleção "tarefas" do Firestore com o valor do input
     try {
       const docRef = await addDoc(collection(db, "tarefas"), {
         nome: value,
@@ -59,7 +51,9 @@ forms.forEach(form => {
       console.error("Erro ao adicionar documento: ", e);
     }
 
-    // Limpa o input do formulário
     input.value = '';
   });
-});
+}
+
+const forms = document.querySelectorAll('form');
+forms.forEach(adicionarItem);
